7bd451c56d90c1c1474459e775ffa00a49bf9b4e
Greg Munro <gregmunro@Websy-Greg.local>
2022-07-12 10:58:06 +0200

    function data

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
-d3.select('body').append('p').text('this is the new paragraph')
-d3.select('body').append('div').insert('h3').text('testing')
-d3.select("body").append("p").text("Hello World!"); 
-d3.select("body")
-  .append("p")
-  .text("Third paragraph");
+// d3.select('body').append('p').text('this is the new paragraph')
+// d3.select('body').append('div').insert('h3').text('testing')
+// d3.select("body").append("p").text("Hello World!"); 
+// d3.select("body")
+//   .append("p")
+//   .text("Third paragraph");
 
-  let data = [100, 200, 300];
+  // let data = [100, 200, 300];
 
-  let paragraph = d3.select("body")
-  .selectAll("p")
-  .data(data)
-  .text(function (d, i) {
-      console.log("data: " + d);
-      console.log("i: " + i);
-      console.log("this: " + this);
+  // let paragraph = d3.select("body")
+  // .selectAll("p")
+  // .data(data)
+  // .text(function (d, i) {
+  //     console.log("data: " + d);
+  //     console.log("i: " + i);
+  //     console.log("this: " + this);
 
-      return d;
+  //     return d;
 
       //this returns the data, 
       //index and html element for each iteration of data
-  });
+  // });
+
+  d3.selectAll('p').style('color', function (d,i) {
+    let text = this.innerText
+    if (text.indexOf('Error') >= 0) {
+      return 'red'
+    } else if (text.indexOf('Warning') >= 0) {
+      return 'yellow'
+    }
+  })
