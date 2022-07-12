dc1b15465134f0e612bd99f03eb36b8e4f1efec1
Greg Munro <gregmunro@Websy-Greg.local>
2022-07-12 11:15:01 +0200

    mouse on and out events

--- index.js
+++ index.js
@@ -31,7 +31,17 @@
   })
 
 d3.select('p')
-  .on('click', function() {
-    d3.select(this)
-    .style('background-color', 'orange')
-  })
+.on("mouseover", function(){
+  d3.select(this)
+    .style("background-color", "orange");
+
+  // Get current event info
+  console.log(d3.event);
+  
+  // Get x & y co-ordinates
+  console.log(d3.mouse(this));
+})
+.on("mouseout", function(){
+  d3.select(this)
+    .style("background-color", "steelblue")
+});
