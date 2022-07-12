e01a3ba81b6eb6015fe7f001b2dfa407bfa1a428
Greg Munro <gregmunro@Websy-Greg.local>
2022-07-12 10:48:04 +0200

    data functions

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 d3.select('body').append('p').text('this is the new paragraph')
 d3.select('body').append('div').insert('h3').text('testing')
+d3.select("body").append("p").text("Hello World!"); 
+d3.select("body")
+  .append("p")
+  .text("Third paragraph");
+
+  let data = [100, 200, 300];
+
+  let paragraph = d3.select("body")
+  .selectAll("p")
+  .data(data)
+  .text(function (d, i) {
+      console.log("data: " + d);
+      console.log("i: " + i);
+      console.log("this: " + this);
+
+      return d;
+
+      //this returns the data, 
+      //index and html element for each iteration of data
+  });
